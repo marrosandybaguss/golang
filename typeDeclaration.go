9ad2fb17e83960ab7797de0db0712d01e4171c94
marrosandybaguss <marrosandybaguss@gmail.com>
2021-09-12 23:26:04 +0700

    type declarations

---
+++ typeDeclaration.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	type KTP string
+	type Merried bool
+
+	var no KTP = "9827598237498273"
+	var name KTP = "Marrosandy Bagus"
+	var status Merried = false
+	fmt.Println(no)
+	fmt.Println(name)
+	fmt.Println(status)
+}
